9f96aef0864337fe01baae90deac84ab0001b164
Samuel Haefs <samuel.haefs@tu-dortmund.de>
2021-07-13 12:10:27 +0200

    auswertung korrigiert python phasengeschw berechnet 2. abgabe datum zu main hinzugefügt

--- V53_Mikrowellen/python/frequenz_aus_wellenlaenge.py
+++ V53_Mikrowellen/python/frequenz_aus_wellenlaenge.py
@@ -4,3 +4,7 @@ lam = 50 *10**(-3) #m
 a = 22.86 * 10**(-3) #m
 f = c * np.sqrt((1/lam)**2 + (1/(2*a)**2))
 print('f =', f*10**(-9), 'GHz')
+
+f_gem = 9007 * 10**6 #Hz
+
+print(f'v_phase = {f_gem*lam} m/s = {f_gem*lam/c} c')
