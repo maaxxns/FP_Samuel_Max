a1586a081b36df17360952c63feb9557524466fb
maaxxns <max.koch@udo.edu>
2021-07-17 09:02:50 +0200

    The detectorscan, with a curve_fit through the measured values and the full with at half max calculated from the curvefit

--- V44_Roentgenreflektrometrie/python/Detectorscan.py
+++ V44_Roentgenreflektrometrie/python/Detectorscan.py
@@ -17,11 +17,31 @@ def gauss(x, a, sigma, mu):
 #curve_fit machen
 popt, cov = curve_fit(gauss, theta, I, p0=(1,1,1) )
 
-x_plot = np.linspace(-0.5, 0.5, 1000)
+x_plot = np.linspace(-0.5, 0.5, 10000)
+
+#Aus der gauss ausgleichsfkt. das maximum der intensität berechen
+I_gauss = gauss(x_plot, *popt)
+I_max = np.amax(I_gauss)
+index = np.argmax(I_gauss)
+x_max = x_plot[index]
+
+#Halbwertsbreite aus ausgleichsfkt. berechnen
+half_max = I_max/2
+cut_off = np.where(I_gauss >= half_max)
+#print(cut_off)
+idx_links = np.min(cut_off)
+idx_rechts = np.max(cut_off)
+print(len(cut_off))
+#print(idx_links, idx_rechts)
+x_half = [x_plot[idx_links], x_plot[idx_rechts]] 
+#print(x_half)
+print(abs(x_half[0]- x_half[1]))
 
 #plotten
 plt.figure()
 plt.plot(theta, I, 'rx',label='Messwerte')
+plt.plot(x_max, I_max,'ko' ,label='Intensitätsaximum')
+plt.plot(x_half, gauss(x_half, *popt), '--', label='Halbwertsbreite')
 plt.plot(x_plot, gauss(x_plot, *popt), 'b-',label='Gaußfunktion')
 plt.grid()
 plt.legend()
